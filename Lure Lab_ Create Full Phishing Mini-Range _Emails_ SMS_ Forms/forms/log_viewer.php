a68b8be9a75783b84881fbeb1982cfe8ee9cff57
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-07-03 19:27:54 -0700

    Create log_viewer.php

---
+++ Lure Lab_ Create Full Phishing Mini-Range _Emails_ SMS_ Forms/forms/log_viewer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * View captured credentials (admin interface)
+ */
+
+// TODO: Add authentication check
+// TODO: Read log files
+// TODO: Parse JSON entries
+// TODO: Display in HTML table
+// TODO: Add filtering by date/type
+// TODO: Include export functionality
+
+?>
